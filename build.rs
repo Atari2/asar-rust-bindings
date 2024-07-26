4b540f4d9f212e5eba03e11402c6b6fe55817aaa
Alessio Rosiello <alex.robassomero@gmail.com>
2024-07-27 01:03:13 +0200

    Don't pull asar during build but include it as a git submodule

--- build.rs
+++ build.rs
@@ -1,5 +1,4 @@
 use cmake::Config;
-use git2::Repository;
 use std::env;
 use std::path::PathBuf;
 
@@ -15,30 +14,12 @@ fn main() {
     // Clone the asar repository and checkout the v1.91 tag
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    let asar_version = env::var("ASAR_VERSION").unwrap_or("v1.91".to_string());
-
-    if !out_dir.join("asar").exists() {
-        let url = "https://github.com/RPGHacker/asar";
-        let asar_repo = Repository::clone(url, out_dir.join("asar")).unwrap();
-        let (object, reference) = asar_repo
-            .revparse_ext(asar_version.as_str())
-            .expect("Failed to get tag");
-        asar_repo
-            .checkout_tree(&object, None)
-            .expect("Failed to checkout tree");
-
-        match reference {
-            Some(gref) => asar_repo.set_head(gref.name().unwrap()),
-            None => asar_repo.set_head_detached(object.id()),
-        }
-        .expect("Failed to set head");
-    }
-
     let expected_lib_path = out_dir.join("lib").join(make_lib_name("asar"));
 
     // build asar with cmake
     if !expected_lib_path.exists() {
-        let _dst = Config::new(out_dir.join("asar/src"))
+        let _dst = Config::new("src/asar/src")
+            .out_dir(out_dir.clone())
             .define("ASAR_GEN_LIB", "ON")
             .define("ASAR_GEN_EXE", "OFF")
             .define("ASAR_GEN_DLL", "OFF")
@@ -48,7 +29,7 @@ fn main() {
             .build();
     }
 
-    println!("cargo:rerun-if-changed=src/asar/asar.h");
+    println!("cargo:rerun-if-changed=src/asar/src/asar-dll-bindings/c/asar.h");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -56,7 +37,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("src/asar/asar.h")
+        .header("src/asar/src/asar-dll-bindings/c/asar.h")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
